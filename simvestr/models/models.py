20c75e453341701b2203993f5e865e7056203c2b
kovidd <49589399+kovidd@users.noreply.github.com>
2020-10-07 20:27:34 +1100

    Add pwd, fname, lname to table User

--- simvestr/models/models.py
+++ simvestr/models/models.py
@@ -7,6 +7,9 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-
+    password = db.Column(db.String(30))
+    first_name = db.Column(db.String(30))
+    last_name = db.Column(db.String(30))
+    
     def __repr__(self):
         return '<User %r>' % self.username
