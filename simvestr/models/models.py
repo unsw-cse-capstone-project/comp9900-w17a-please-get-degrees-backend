f2703887377d93dc1dbd9cd795b3399f9cf90563
kovidd <49589399+kovidd@users.noreply.github.com>
2020-10-07 21:01:27 +1100

    Update models.py

--- simvestr/models/models.py
+++ simvestr/models/models.py
@@ -6,7 +6,7 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    email_id = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(30))
     first_name = db.Column(db.String(30))
     last_name = db.Column(db.String(30))
