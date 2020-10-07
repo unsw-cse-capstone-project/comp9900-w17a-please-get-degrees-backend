05edd419488dbf18ea35819f50cc87c0276441e5
Khan Schroder-Turner <khan.schroder-turner@ghd.com>
2020-10-08 07:23:53 +1100

    Added licence and updated setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,27 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='simvestr',
-    version='1.0.0',
+    version='0.0.1',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=[
         'flask',
     ],
+    author="Jihad Meraachli; Khan Schroder-Turner; Kovid Sharma; Simon Garrod; Timothy Brunette",
+    author_email="simvestr@gmail.com",
+    description="A small example package",
+    url='https://github.com/unsw-cse-capstone-project/comp9900-w17a-please-get-degrees-backend',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
 )
