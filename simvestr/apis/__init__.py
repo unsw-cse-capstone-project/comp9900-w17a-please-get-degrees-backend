2ce02cfea5710dc162745ec10a0803dea80be8db
kovidd <49589399+kovidd@users.noreply.github.com>
2020-10-07 21:24:12 +1100

    Adding signup and token

--- simvestr/apis/__init__.py
+++ simvestr/apis/__init__.py
@@ -3,6 +3,8 @@ from flask_restx import Api
 #Import the api variable from each of the namespaces (api files) and alias them
 from .namespace1 import api as ns1
 from .user import api as ns2
+from .signup import api as ns3
+from .token import api as ns4
 
 from flask import Blueprint
 
@@ -18,3 +20,5 @@ api = Api(blueprint,
 #Register each new namespace here
 api.add_namespace(ns1,  path='/hello/')
 api.add_namespace(ns2,  path='/user/')
+api.add_namespace(ns3,  path='/signup/')
+api.add_namespace(ns4,  path='/token/')
