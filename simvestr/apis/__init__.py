7023b9e10af2da9e2939290d6d822f5e4f2a04a8
KhanMechAI <khan.schroderturner@gmail.com>
2020-10-10 12:14:32 +1100

    Mostly complete search api. needs some filtering/pagination/local storage to limit data transfers

--- simvestr/apis/__init__.py
+++ simvestr/apis/__init__.py
@@ -6,10 +6,11 @@ from .user import api as ns2
 from .signup import api as ns3
 from .token import api as ns4
 from .forgotuser import api as ns5
+from .search import api as ns6
 
 from flask import Blueprint
 
-blueprint = Blueprint('api', __name__, url_prefix='/api')
+blueprint = Blueprint('api', __name__, url_prefix='/api/v1')
 
 api = Api(blueprint,
     title='SimvestrAPI',
@@ -19,8 +20,9 @@ api = Api(blueprint,
 )
 
 #Register each new namespace here
-api.add_namespace(ns1,  path='/hello')
-api.add_namespace(ns2,  path='/user')
-api.add_namespace(ns3,  path='/signup')
-api.add_namespace(ns4,  path='/token')
-api.add_namespace(ns5,  path='/forgotuser')
+api.add_namespace(ns1, path='/hello')
+api.add_namespace(ns2, path='/user')
+api.add_namespace(ns3, path='/signup')
+api.add_namespace(ns4, path='/token')
+api.add_namespace(ns5, path='/forgotuser')
+api.add_namespace(ns6, path='/search')
