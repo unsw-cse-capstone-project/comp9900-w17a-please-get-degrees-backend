97016fb67ee419f5123ba46896e2ec3cb88ea894
Simon <simon@sventhesnake.com>
2020-10-13 09:28:44 +1100

    added stocks.py with api endpoints for stocks

--- simvestr/apis/stocks.py
+++ simvestr/apis/stocks.py
@@ -1,7 +1,9 @@
 from flask_restx import Resource, fields, reqparse, Namespace
 from ..models import Stock
-from flask import jsonify
+from flask import jsonify, make_response
 from simvestr.models import db
+import requests
+import json
 
 api = Namespace('Stocks', description='Demo api for querying stocks')
 
@@ -32,12 +34,16 @@ class put_pstock(Resource):
         db.session.commit()
         return jsonify({'message': 'New stock created!'})
 
+# gets stock data from simvestr database
 
-@ api.route('/<symbol>')
+
+@ api.route('/stored/<symbol>')
 class get_stock(Resource):
     def get(self, symbol):
         stock = Stock.query.get(symbol)
         return jsonify(stock=Stock.serialize(stock))
+
+# gets ALL data from simvestr database
 
 
 @ api.route('/all')
@@ -45,3 +51,40 @@ class get_stocks(Resource):
     def get(self):
         stocks = Stock.query.all()
         return jsonify(stocks=Stock.serialize_list(stocks))
+
+# Simon Test -- this populates the stocks table from finnhub
+# @ api.route('/populate')
+# class populate(Resource):
+#     def get(self):
+
+#         r = requests.get(
+#             'https://finnhub.io/api/v1/stock/symbol?exchange=US&token=bu1uf5748v6sao5m33sg')
+#         stocks = r.json()
+#         Stock.query.delete()
+#         for stock in stocks:
+#             symbol = stock['symbol']
+#             name = stock['symbol']+stock['description']
+#             currency = stock['currency']
+#             exchange = '-'
+#             industry = stock['type']
+#             country = '-'
+#             new_stock = Stock(symbol=symbol, name=name, currency=currency,
+#                               exchange=exchange, industry=industry, country=country)
+#             db.session.add(new_stock)
+#         db.session.commit()
+#         return jsonify(stocks[1400])
+
+# gets stock data from finnhub from a stock symbol
+
+
+@ api.route('/finnhub/<symbol>')
+class get_stock_finn(Resource):
+    def get(self, symbol):
+        r = requests.get(
+            'https://finnhub.io/api/v1/stock/profile2?symbol='+symbol+'&token=bu22pqf48v6uohspr3p0')
+        stock = r.json()
+        r = requests.get(
+            'https://finnhub.io/api/v1/quote?symbol='+symbol+'&token=bu22pqf48v6uohspr3p0')
+        stock.update(r.json())
+
+        return jsonify(stock)
