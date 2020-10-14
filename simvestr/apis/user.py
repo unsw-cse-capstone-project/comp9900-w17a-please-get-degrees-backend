c2a5180a33ef48e8f375c5aebdae7f8413105d0a
kovidd <49589399+kovidd@users.noreply.github.com>
2020-10-15 00:31:55 +1100

    Update user.py

--- simvestr/apis/user.py
+++ simvestr/apis/user.py
@@ -6,7 +6,7 @@ api = Namespace('user', description='Demo api for querying users')
 class UsersQuery(Resource):
     def get(self):
         user = User.query.all()
-        data = {u.id:dict(username=u.username, email=u.email_id, role=u.role, fname=u.first_name, lname=u.last_name, validated=u.validated) for u in user}
+        data = {u.id:dict(email=u.email_id, role=u.role, fname=u.first_name, lname=u.last_name, validated=u.validated) for u in user}
         payload = dict(
             data=data
         )
@@ -18,7 +18,7 @@ class UserQuery(Resource):
     def get(self, user_id: int):
         user = User.query.filter_by(id=user_id).first()
         watch = Watchlist.query.filter(Watchlist.user_id==user.id).all()
-        data = dict(username=user.username, email=user.email_id, watchlist=[s.stock_symbol for s in watch])
+        data = dict(email=user.email_id, watchlist=[s.stock_symbol for s in watch])
         payload = dict(
             data=data
         )
