af4b3419fd5a69f7f1d6ffa041820ad97506a174
Tim Brunette <timothybrunette@gmail.com>
2020-10-14 15:51:53 +1100

    fix signup api issue w/ the arguements being in the query

--- simvestr/apis/signup.py
+++ simvestr/apis/signup.py
@@ -10,68 +10,93 @@ from flask_restx import Resource, fields, reqparse, Namespace
 from werkzeug.security import generate_password_hash
 
 from simvestr_email import send_email
+
 # from simvestr import create_app
 from ..models import User
 from simvestr.models import db
 
 api = Namespace(
-    'Signup',
-    authorizations = {'TOKEN-BASED': {'name': 'API-TOKEN', 'in': 'header', 'type': 'apiKey'}},
-    security = 'TOKEN-BASED',
-    default = 'User Login and Authentication',
-    title = 'Simvestr',
-    description = 'Back-end API User signup and authentication'
+    "signup",
+    authorizations={
+        "TOKEN-BASED": {"name": "API-TOKEN", "in": "header", "type": "apiKey"}
+    },
+    security="TOKEN-BASED",
+    default="User Login and Authentication",
+    title="Simvestr",
+    description="Back-end API User signup and authentication",
 )
 
 # ---------------- Signup new user ----------- #
-signup_model = api.model('signup', {
-    'username': fields.String,
-    'email_id': fields.String,
-    'password': fields.String,
-    'first_name':fields.String,
-    'last_name':fields.String,
-    'other_name':fields.String
-})
+signup_model = api.model(
+    "Signup",
+    {
+        "username": fields.String,
+        "email_id": fields.String,
+        "password": fields.String,
+        "first_name": fields.String,
+        "last_name": fields.String,
+        "other_name": fields.String,
+    },
+)
 signup_parser = reqparse.RequestParser()
-signup_parser.add_argument('username', type=str)
-signup_parser.add_argument('email_id', type=str)
-signup_parser.add_argument('password', type=str)
-signup_parser.add_argument('first_name', type=str)
-signup_parser.add_argument('last_name', type=str)
-signup_parser.add_argument('other_name', type=str)
-@api.route('/')
+signup_parser.add_argument("username", type=str)
+signup_parser.add_argument("email_id", type=str)
+signup_parser.add_argument("password", type=str)
+signup_parser.add_argument("first_name", type=str)
+signup_parser.add_argument("last_name", type=str)
+signup_parser.add_argument("other_name", type=str)
+
+
+@api.route("")
 class Signup(Resource):
-    @api.response(200, 'Successful')
-    @api.response(444, 'User already exists')
-    @api.response(445, 'Email ID already exists')
-    @api.response(446, 'Username should be atleast 8 characters')
-    @api.response(447, 'Password should be atleast 8 characters')
-    @api.doc(description="Creates a new user")
-    @api.expect(signup_parser, validate=True)
+    @api.response(200, "Successful")
+    @api.response(444, "User already exists")
+    @api.response(445, "Email ID already exists")
+    @api.response(446, "Username should be at least 8 characters")
+    @api.response(447, "Password should be at least 8 characters")
+    @api.doc(model="Signup", body=signup_model, description="Creates a new user")
     def post(self):
         args = signup_parser.parse_args()
-        username = args.get('username')
-        email = args.get('email_id')
-        password = args.get('password')
-        fname=args.get('first_name')
-        lname=args.get('last_name')
-        oname=args.get('other_name')
+        username = args.get("username")
+        email = args.get("email_id")
+        password = args.get("password")
+        fname = args.get("first_name")
+        lname = args.get("last_name")
+        oname = args.get("other_name")
         user = User.query.filter_by(username=username).first()
         email_check = User.query.filter_by(email_id=email).first()
-        print("User",user)
-        print("Email",email)
+        print("User", user)
+        print("Email", email)
         if user:
-              return {'error': 'User already exists'}, 444
+            return {"error": True, "message": "User already exists"}, 444
         if email_check:
-              return {'error': 'Email ID already exists'}, 445
+            return {"error": True, "message": "Email ID already exists"}, 445
         if len(username) < 8:
-            return {'error' : 'Username should be atleast 8 characters'}, 446
+            return (
+                {"error": True, "message": "Username should be at least 8 characters"},
+                446,
+            )
         if len(password) < 8:
-            return {'error' : 'Password should be atleast 8 characters'}, 447
-        new_user = User(username=username, email_id=email, first_name=fname, last_name=lname, other_name=oname, role='user', password=generate_password_hash(password, method='sha256'))
+            return (
+                {"error": True, "message": "Password should be at least 8 characters",},
+                447,
+            )
+        new_user = User(
+            username=username,
+            email_id=email,
+            first_name=fname,
+            last_name=lname,
+            other_name=oname,
+            role="user",
+            password=generate_password_hash(password, method="sha256"),
+        )
         db.session.add(new_user)
         db.session.commit()
-        message_content = 'A new user from your email ID has signed-up for our free investing simlutor. Please login to start investing'
-        send_email(email, 'User created successfully', message_content) #sends a confirmation email to the user
-        return jsonify({'message' : 'New user created!'})
+        message_content = "A new user from your email ID has signed-up for our free investing simlutor. Please login to start investing"
+        send_email(
+            email, "User created successfully", message_content
+        )  # sends a confirmation email to the user
+        return {"error": False, "message": "New user created!"}, 200
+
+
 # ---------------- Signup new user ----------- #
