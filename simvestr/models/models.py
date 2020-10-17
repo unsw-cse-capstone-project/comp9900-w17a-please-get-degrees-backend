930f59de3431c37aeffa5b19330ad73495c1d8b3
kovidd <49589399+kovidd@users.noreply.github.com>
2020-10-17 14:36:12 +1100

    Adding Portfolio, Price, Transaction classes

--- simvestr/models/models.py
+++ simvestr/models/models.py
@@ -35,10 +35,15 @@ class User(db.Model):
     last_updated = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
     validated = db.Column(db.Boolean, default=False)
     role = db.Column(ChoiceType(ROLE_CHOICES))
+    
     watchlist = db.relationship("Watchlist", backref='user', lazy='dynamic', cascade="all, delete-orphan",)#need similar for portfolio, except it will be 1-m
+    portfolio = db.relationship("Portfolio", backref='user', lazy='dynamic', cascade="all, delete-orphan",)
+    portfolioprice = db.relationship("PortfolioPrice", backref='user', lazy='dynamic', cascade="all, delete-orphan",)
+    transaction = db.relationship("Transaction", backref='user', lazy='dynamic', cascade="all, delete-orphan",)
     
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email_id
+
 
 class Watchlist(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -57,3 +62,41 @@ class Stock(db.Model):
     exchange = db.Column(db.String(200),)
     industry = db.Column(db.String(120),)
     country = db.Column(db.String(120),)
+
+
+class Portfolio(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    portfolio_id = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    portfolio_name = db.Column(db.String(30), nullable=False)
+    portfolio_price = db.relationship("PortfolioPrice", backref='portfolio', lazy='dynamic', cascade="all, delete-orphan",)
+    transaction = db.relationship("Transaction", backref='portfolio', lazy='dynamic', cascade="all, delete-orphan",)
+        
+    def __repr__(self):
+        return '<Portfolio %r>' % self.portfolio_name
+    
+    
+class PortfolioPrice(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    portfolio_id = db.Column(db.Integer, db.ForeignKey('portfolio.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    close_balance = db.Column(db.Integer)
+    timestamp = db.Column(db.DateTime, default=datetime.now,)
+    
+    
+class Transaction(db.Model):
+    TRADE_CHOICES = dict(
+        buy='buy',
+        sell='sell',
+        settled='settled'
+    )
+    
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    portfolio_id = db.Column(db.Integer, db.ForeignKey('portfolio.portfolio_id'))
+    symbol = db.Column(db.String(6), nullable=False)
+    cost = db.Column(db.Integer, nullable=False)
+    trade_type = db.Column(ChoiceType(TRADE_CHOICES))
+    timestamp = db.Column(db.DateTime, default=datetime.now,)
+    quantity = db.Column(db.Integer, nullable=False)
+    fee = db.Column(db.Integer, default=0)
