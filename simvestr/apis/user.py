114fcd1df2c5aa0b16d6753d28bf088b9d122541
kovidd <49589399+kovidd@users.noreply.github.com>
2020-10-17 14:55:15 +1100

    Minor spacing changes

--- simvestr/apis/user.py
+++ simvestr/apis/user.py
@@ -1,20 +1,20 @@
 from flask_restx import Resource, Namespace
 from ..models import User, Watchlist, Stock, Transaction, Portfolio
-api = Namespace('view user details', description='Demo api for querying users')
+api = Namespace('view user details', description = 'Demo api for querying users')
 
 @api.route('/')
 class UsersQuery(Resource):
     def get(self):
         user = User.query.all()
         data = {u.id:dict
-                (email=u.email_id,
-                 role=u.role,
-                 fname=u.first_name,
-                 lname=u.last_name,
-                 validated=u.validated,
+                (email = u.email_id,
+                 role = u.role,
+                 fname = u.first_name,
+                 lname = u.last_name,
+                 validated = u.validated,
                  ) for u in user}
         payload = dict(
-            data=data
+            data = data
         )
         return payload
 
@@ -22,15 +22,15 @@ class UsersQuery(Resource):
 @api.route('/<int:user_id>')
 class UserQuery(Resource):
     def get(self, user_id: int):
-        user = User.query.filter_by(id=user_id).first()
-        watch = Watchlist.query.filter(Watchlist.user_id==user.id).all()
-        port = Portfolio.query.filter(Portfolio.user_id==user.id).all()
-        transact = Transaction.query.filter(Transaction.user_id==user.id).all()
-        data = dict(email=user.email_id, 
+        user = User.query.filter_by(id = user_id).first()
+        watch = Watchlist.query.filter(Watchlist.user_id == user.id).all()
+        port = Portfolio.query.filter(Portfolio.user_id == user.id).all()
+        transact = Transaction.query.filter(Transaction.user_id == user.id).all()
+        data = dict(email = user.email_id, 
                     watchlist = [s.stock_symbol for s in watch], 
                     portfolio = [p.portfolio_name for p in port],
-                    transaction=[(t.symbol, t.trade_type, t.quantity) for t in transact])
+                    transaction = [(t.symbol, t.trade_type, t.quantity) for t in transact])
         payload = dict(
-            data=data
+            data = data
         )
         return payload
