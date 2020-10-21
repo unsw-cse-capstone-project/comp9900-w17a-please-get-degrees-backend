dcf8565d41443e5e541f05d2a71292454230be96
Khan Schroder-Turner <khan.schroder-turner@ghd.com>
2020-10-22 10:16:56 +1100

    Query all stocks endpoint and cleaned up the funnhub uri's. Still workign on search to query from db. will be done soon

--- simvestr/apis/search.py
+++ simvestr/apis/search.py
@@ -1,35 +1,83 @@
-from flask_restx import Resource, Namespace
+from flask_restx import Resource, Namespace, reqparse
 from ..models import User, Watchlist, Stock
 from flask import jsonify, current_app
 import requests
 
 api = Namespace("search", description="Search stocks")
 
+FINNHUB_BASE = 'https://finnhub.io/api/v1/'
+
+QUERYS = dict(
+    exchange="stock/symbol?exchange=",
+    profile="stock/profile2?symbol=",
+    quote="quote?symbol="
+)
+
+STOCK_TYPE_MAP = {
+    True:"CRYPTO",
+    False:"STOCK"
+}
+
+def finnhub_search(query:str, arg):
+    token = f'&token={current_app.config["FINNHUB_API_KEY"]}'
+    query_string = QUERYS[query]
+    uri = f'{FINNHUB_BASE}{query_string}{arg}{token}'
+    return uri
 
 @api.route("/exchange/<string:exchange>")
-class StockList(Resource):
+class ExchangeList(Resource):
     def get(self, exchange: str = "US"):
-        SYMBOL_ALL_API = (
-            lambda ex: f'https://finnhub.io/api/v1/stock/symbol?exchange={ex}&token={current_app.config["FINNHUB_API_KEY"]}'
+        uri = finnhub_search(
+            query="exchange",
+            arg=exchange
         )
-
-        r = requests.get(SYMBOL_ALL_API(exchange))
+        r = requests.get(uri)
         return r.json()
 
 
 @api.route("/symbol/<string:stock_symbol>")
 class StockDetails(Resource):
+    @api.response(200, "Success")
+    @api.response(404, "Symbol not found")
     def get(self, stock_symbol: str = "APPL"):
+
+        stock_q = Stock.query.filter_by(display_symbol=stock_symbol).all()
         # This can be STOCK or CRYPTO
         stockType = "STOCK"
-        PROFILE_API = (
-            lambda sym: f'https://finnhub.io/api/v1/stock/profile2?symbol={sym}&token={current_app.config["FINNHUB_API_KEY"]}'
+        if not stock_q:
+            uri = finnhub_search(
+                query="profile",
+                arg=stock_symbol
+            )
+            stock = requests.get(uri).json()
+        else:
+            return [
+                dict(
+                    type=STOCK_TYPE_MAP[s.is_crypto],
+                    symbol=s.display_symbol,
+                    name=s.name,
+                    exchage=s.exchange,
+                    marketCapitalization=requests.get(finnhub_search(
+                        query="profile",
+                        arg=s.symbol
+                    )).json()["marketCapitalization"],
+                    quote=requests.get(finnhub_search(
+                        query='quote',
+                        arg=stock_symbol
+                    )).json()
+                )
+            for s in stock_q]
+
+        if not stock:
+            return {
+                "error":True,
+                "message": "Symbol not found. Incorrect symbol, check spelling."
+            }
+        uri = finnhub_search(
+            query="quote",
+            arg=stock_symbol
         )
-        stock = requests.get(PROFILE_API(stock_symbol)).json()
-        QUOTE_API = (
-            lambda sym: f'https://finnhub.io/api/v1/quote?symbol={sym}&token={current_app.config["FINNHUB_API_KEY"]}'
-        )
-        r = requests.get(QUOTE_API(stock_symbol)).json()
+        r = requests.get(uri).json()
         stock.update({"type": stockType, "quote": r})
         return jsonify(stock)
 
@@ -37,9 +85,11 @@ class StockDetails(Resource):
 @api.route("/symbols")
 class StockSymbols(Resource):
     def get(self, exchange: str = "US"):
-        PROFILE_API = (
-            lambda ex: f'https://finnhub.io/api/v1/stock/symbol?exchange={ex}&token={current_app.config["FINNHUB_API_KEY"]}'
-        )
-        stocks = requests.get(PROFILE_API(exchange)).json()
-        payload = [stock["symbol"] for stock in stocks]
-        return jsonify(payload)
+        stock_q = Stock.query.all()
+        return [
+                dict(
+                    symbol=s.symbol,
+                    display_symbol=s.display_symbol,
+                    name=s.name,
+                )
+            for s in stock_q]
