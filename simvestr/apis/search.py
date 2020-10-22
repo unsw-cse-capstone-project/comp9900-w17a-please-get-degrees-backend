e76181f46bc3f6962fdb2d90d01125e2230eb558
Tim Brunette <timothybrunette@gmail.com>
2020-10-22 11:26:26 +1100

    add search filter similiarity on the symbol

--- simvestr/apis/search.py
+++ simvestr/apis/search.py
@@ -5,50 +5,44 @@ import requests
 
 api = Namespace("search", description="Search stocks")
 
-FINNHUB_BASE = 'https://finnhub.io/api/v1/'
+FINNHUB_BASE = "https://finnhub.io/api/v1/"
 
 QUERYS = dict(
     exchange="stock/symbol?exchange=",
     profile="stock/profile2?symbol=",
-    quote="quote?symbol="
+    quote="quote?symbol=",
 )
 
-STOCK_TYPE_MAP = {
-    True:"CRYPTO",
-    False:"STOCK"
-}
+STOCK_TYPE_MAP = {True: "CRYPTO", False: "STOCK"}
 
-def finnhub_search(query:str, arg):
+
+def finnhub_search(query: str, arg):
     token = f'&token={current_app.config["FINNHUB_API_KEY"]}'
     query_string = QUERYS[query]
-    uri = f'{FINNHUB_BASE}{query_string}{arg}{token}'
+    uri = f"{FINNHUB_BASE}{query_string}{arg}{token}"
     return uri
+
 
 @api.route("/exchange/<string:exchange>")
 class ExchangeList(Resource):
     def get(self, exchange: str = "US"):
-        uri = finnhub_search(
-            query="exchange",
-            arg=exchange
-        )
+        uri = finnhub_search(query="exchange", arg=exchange)
         r = requests.get(uri)
         return r.json()
 
 
-@api.route("/symbol/<string:stock_symbol>")
+@api.route("/details/<string:stock_symbol>")
 class StockDetails(Resource):
     @api.response(200, "Success")
     @api.response(404, "Symbol not found")
+    @api.doc(description="Gets details for the specified stock",)
     def get(self, stock_symbol: str = "APPL"):
 
         stock_q = Stock.query.filter_by(display_symbol=stock_symbol).all()
         # This can be STOCK or CRYPTO
         stockType = "STOCK"
         if not stock_q:
-            uri = finnhub_search(
-                query="profile",
-                arg=stock_symbol
-            )
+            uri = finnhub_search(query="profile", arg=stock_symbol)
             stock = requests.get(uri).json()
         else:
             return [
@@ -57,29 +51,35 @@ class StockDetails(Resource):
                     symbol=s.display_symbol,
                     name=s.name,
                     exchage=s.exchange,
-                    marketCapitalization=requests.get(finnhub_search(
-                        query="profile",
-                        arg=s.symbol
-                    )).json()["marketCapitalization"],
-                    quote=requests.get(finnhub_search(
-                        query='quote',
-                        arg=stock_symbol
-                    )).json()
+                    marketCapitalization=requests.get(
+                        finnhub_search(query="profile", arg=s.symbol)
+                    ).json()["marketCapitalization"],
+                    quote=requests.get(
+                        finnhub_search(query="quote", arg=stock_symbol)
+                    ).json(),
                 )
-            for s in stock_q]
+                for s in stock_q
+            ]
 
         if not stock:
             return {
-                "error":True,
-                "message": "Symbol not found. Incorrect symbol, check spelling."
+                "error": True,
+                "message": "Symbol not found. Incorrect symbol, check spelling.",
             }
-        uri = finnhub_search(
-            query="quote",
-            arg=stock_symbol
-        )
+        uri = finnhub_search(query="quote", arg=stock_symbol)
         r = requests.get(uri).json()
         stock.update({"type": stockType, "quote": r})
         return jsonify(stock)
+
+
+@api.route("/<string:name>")
+class StockSearch(Resource):
+    def get(self, name: str = "APPL"):
+        stock_q = Stock.query.filter(Stock.display_symbol.ilike(name + "%")).all()
+        return [
+            dict(symbol=s.symbol, display_symbol=s.display_symbol, name=s.name,)
+            for s in stock_q
+        ]
 
 
 @api.route("/symbols")
@@ -87,9 +87,6 @@ class StockSymbols(Resource):
     def get(self, exchange: str = "US"):
         stock_q = Stock.query.all()
         return [
-                dict(
-                    symbol=s.symbol,
-                    display_symbol=s.display_symbol,
-                    name=s.name,
-                )
-            for s in stock_q]
+            dict(symbol=s.symbol, display_symbol=s.display_symbol, name=s.name,)
+            for s in stock_q
+        ]
