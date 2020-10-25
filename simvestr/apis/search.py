f75c4ff2488e96e2a0e25551d7815c65f3f19f70
Khan Schroder-Turner <khan.schroder-turner@ghd.com>
2020-10-25 12:12:35 +1100

    Updated search to be more general

--- simvestr/apis/search.py
+++ simvestr/apis/search.py
@@ -1,31 +1,18 @@
-from flask_restx import Resource, Namespace, reqparse, fields
-from ..models import User, Watchlist, Stock
-from flask import jsonify, current_app
 import requests
+
+from flask_restx import Resource, Namespace, reqparse, fields
+from flask import jsonify, current_app
+
+from simvestr.models import User, Watchlist, Stock
+from simvestr.helpers.search import search, STOCK_TYPE_MAP
 
 api = Namespace("search", description="Search stocks")
 
-FINNHUB_BASE = "https://finnhub.io/api/v1/"
-
-QUERYS = dict(
-    exchange="stock/symbol?exchange=",
-    profile="stock/profile2?symbol=",
-    quote="quote?symbol=",
-)
-
-STOCK_TYPE_MAP = {True: "CRYPTO", False: "STOCK"}
-
-
-def finnhub_search(query: str, arg):
-    token = f'&token={current_app.config["FINNHUB_API_KEY"]}'
-    query_string = QUERYS[query]
-    uri = f"{FINNHUB_BASE}{query_string}{arg}{token}"
-    return uri
 
 @api.route("/exchange/<string:exchange>")
 class ExchangeList(Resource):
     def get(self, exchange: str = "US"):
-        uri = finnhub_search(query="exchange", arg=exchange)
+        uri = search(source_api='finnhub' ,query="exchange", arg=exchange)
         r = requests.get(uri)
         return r.json()
 
@@ -39,6 +26,14 @@ quote_model = api.model(
         "pc": fields.Float,
     }
 )
+base_symbol_model = api.model(
+    "Symbol", {
+        "type": fields.String,
+        "symbol": fields.String,
+        "display_symbol": fields.String,
+        "name": fields.String,
+    }
+)
 symbol_model = api.model(
     "Symbol", {
         "type": fields.String,
@@ -50,6 +45,7 @@ symbol_model = api.model(
     }
 )
 
+
 @api.route("/details/<string:stock_symbol>")
 class StockDetails(Resource):
     response_fields = [
@@ -60,6 +56,7 @@ class StockDetails(Resource):
         "marketCapitalization",
         "quote",
     ]
+    @api.marshal_with(symbol_model)
     @api.param('stock_symbol', 'Stock or crypto symbol to be searched')
     @api.response(200, "Success")
     @api.response(404, "Symbol not found")
@@ -70,14 +67,15 @@ class StockDetails(Resource):
         stock_q = Stock.query.filter_by(display_symbol=stock_symbol.upper()).all()
 
         stockType = STOCK_TYPE_MAP[False]
+        print("hit the API")
         if not stock_q:
-            uri = finnhub_search(query="profile", arg=stock_symbol)
+            uri = search(source_api='finnhub' ,query="profile", arg=stock_symbol)
             stock = requests.get(uri).json()
 
             # If not, it will hit the crypto api to see if the stock is there.
             if not stock:
                 stockType = STOCK_TYPE_MAP[True]
-                uri = finnhub_search(
+                uri = search(source_api='finnhub' ,
                     query="profile",
                     arg=stock_symbol
                 )
@@ -90,10 +88,10 @@ class StockDetails(Resource):
                     name=s.name,
                     exchage=s.exchange,
                     marketCapitalization=requests.get(
-                        finnhub_search(query="profile", arg=s.symbol)
+                        search(source_api='finnhub' ,query="profile", arg=s.symbol)
                     ).json()["marketCapitalization"],
                     quote=requests.get(
-                        finnhub_search(query="quote", arg=stock_symbol)
+                        search(source_api='finnhub' ,query="quote", arg=stock_symbol)
                     ).json(),
                 )
                 for s in stock_q
@@ -105,21 +103,21 @@ class StockDetails(Resource):
                 "error": True,
                 "message": "Symbol not found. Incorrect symbol, check spelling.",
             }
-        uri = finnhub_search(query="quote", arg=stock_symbol)
+        uri = search(source_api='finnhub' ,query="quote", arg=stock_symbol)
         r = requests.get(uri).json()
         stock.update({"type": stockType, "quote": r})
-        stock = {k:v for k, v in stock if k in StockDetails.response_fields}
+        stock = {k: v for k, v in stock if k in StockDetails.response_fields}
         return stock
 
 
-@api.route("/<string:name>")
-class StockSearch(Resource):
-    def get(self, name: str = "APPL"):
-        stock_q = Stock.query.filter(Stock.display_symbol.ilike(name + "%")).all()
-        return [
-            dict(symbol=s.symbol, display_symbol=s.display_symbol, name=s.name,)
-            for s in stock_q
-        ]
+# @api.route("/<string:name>")
+# class StockSearch(Resource):
+#     def get(self, name: str = "APPL"):
+#         stock_q = Stock.query.filter(Stock.display_symbol.ilike(name + "%")).all()
+#         return [
+#             dict(symbol=s.symbol, display_symbol=s.display_symbol, name=s.name, )
+#             for s in stock_q
+#         ]
 
 
 @api.route("/symbols")
@@ -127,6 +125,6 @@ class StockSymbols(Resource):
     def get(self, exchange: str = "US"):
         stock_q = Stock.query.all()
         return [
-            dict(symbol=s.symbol, display_symbol=s.display_symbol, name=s.name,)
+            dict(symbol=s.symbol, display_symbol=s.display_symbol, name=s.name, )
             for s in stock_q
         ]
