038680e3f2e10a9c747a97939ae0578fd5eb8cf9
simonxxxxx <testsg@cse.unsw.edu.au>
2020-10-27 09:38:27 +1100

    add leaderboard endpoints

--- simvestr/apis/__init__.py
+++ simvestr/apis/__init__.py
@@ -1,6 +1,6 @@
 from flask_restx import Api
 
-# Import the api variable from each of the namespaces (api files) and alias them
+#Import the api variable from each of the namespaces (api files) and alias them
 from .user import api as ns1
 from .signup import api as ns2
 from .token import api as ns3
@@ -13,31 +13,30 @@ from .viewportfolio import api as ns9
 from .viewbalance import api as ns10
 from .viewtransaction import api as ns11
 from .changedetails import api as ns12
-
+from .leaderboard import api as ns13
 
 from flask import Blueprint
 
-blueprint = Blueprint("api", __name__, url_prefix="/api/v1")
+blueprint = Blueprint('api', __name__, url_prefix='/api/v1')
 
-
-api = Api(
-    blueprint,
-    title="SimvestrAPI",
-    version="0.01",
-    description="Simvestr api",
+api = Api(blueprint,
+    title='SimvestrAPI',
+    version='0.01',
+    description='Simvestr api',
     # All API metadatas
 )
 
-# Register each new namespace here
-api.add_namespace(ns1, path="/user")
-api.add_namespace(ns2, path="/signup")
-api.add_namespace(ns3, path="/token")
-api.add_namespace(ns4, path="/verifytoken")
-api.add_namespace(ns5, path="/forgotuser")
-api.add_namespace(ns6, path="/search")
-api.add_namespace(ns7, path="/marketorder")
-api.add_namespace(ns8, path="/viewstocksowned")
-api.add_namespace(ns9, path="/viewportfolio")
-api.add_namespace(ns10, path="/viewbalance")
-api.add_namespace(ns11, path="/viewtransaction")
-api.add_namespace(ns12, path="/changedetails")
+#Register each new namespace here
+api.add_namespace(ns1, path='/user')
+api.add_namespace(ns2, path='/signup')
+api.add_namespace(ns3, path='/token')
+api.add_namespace(ns4, path='/verifytoken')
+api.add_namespace(ns5, path='/forgotuser')
+api.add_namespace(ns6, path='/search')
+api.add_namespace(ns7, path='/marketorder')
+api.add_namespace(ns8, path='/viewstocksowned')
+api.add_namespace(ns9, path='/viewportfolio')
+api.add_namespace(ns10, path='/viewbalance')
+api.add_namespace(ns11, path='/viewtransaction')
+api.add_namespace(ns12, path='/changedetails')
+api.add_namespace(ns13, path='/leaderboard')
---
+++ simvestr/apis/leaderboard.py
@@ -0,0 +1,68 @@
+from simvestr.models import db
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import func, and_, join
+from flask import jsonify
+from flask_restx import Resource, Namespace
+from sqlalchemy.sql import select
+from ..models import User, Watchlist, Stock, Portfolio, PortfolioPrice
+
+api = Namespace('leader board', description='Leaderboard')
+
+
+@api.route('/position/<int:user_id>')
+class PortfolioQuery(Resource):
+    @api.response(200, 'Successful')
+    @api.response(602, 'PortfolioPrice doesn\'t exist')
+    def get(self, user_id: int):
+        subq = db.session.query(
+            PortfolioPrice.user_id,
+            func.max(PortfolioPrice.timestamp).label('maxdate')
+        ).group_by(PortfolioPrice.user_id).subquery('t2')
+        
+        close_balances = []
+        for p in db.session.query(PortfolioPrice).join(subq, and_(
+                PortfolioPrice.user_id == subq.c.user_id,
+                PortfolioPrice.timestamp == subq.c.maxdate
+            )
+        ):
+            close_balances.append({"user": p.user_id, "close_balance": p.close_balance})
+        balances_sorted = sorted(close_balances, key=lambda i: i['close_balance'], reverse=True)
+        
+        pos = 1
+        while (balances_sorted[pos - 1]['user'] != user_id):
+            pos += 1
+        suffix = ['th', 'st', 'nd', 'rd', 'th'][min(pos % 10, 4)]
+        if 11 <= (pos % 100) <= 13:
+            suffix = 'th'
+        return jsonify(str(pos) + suffix)
+
+
+@api.route('/top/<int:num_ports>')
+class TopInvestors(Resource):
+    @api.response(200, 'Successful')
+    @api.response(602, 'PortfolioPrice doesn\'t exist')
+    def get(self, num_ports: int = 6):
+        subq = db.session.query(
+            PortfolioPrice.user_id,
+            func.max(PortfolioPrice.timestamp).label('maxdate')
+        ).group_by(PortfolioPrice.user_id).subquery('t2')
+        
+        close_balances = []
+        for p in db.session.query(PortfolioPrice).join(subq, and_(
+            PortfolioPrice.user_id == subq.c.user_id,
+            PortfolioPrice.timestamp == subq.c.maxdate)
+        ):
+            close_balances.append({"user": p.user_id, "close_balance": p.close_balance})
+        balances_sorted = sorted(close_balances, key=lambda i: i['close_balance'], reverse=True)
+
+        users = []
+        idAndBalance = {}
+        for i in range(num_ports):
+            idAndBalance.update({balances_sorted[i]['user']: balances_sorted[i]['close_balance']})
+            users.append(balances_sorted[i]['user'])
+        top_portfolios = []
+        for p in db.session.query(Portfolio).join(Portfolio.user).filter(Portfolio.user_id.in_(users)).all():
+            top_portfolios.append({"id": p.user_id, "user": p.user.first_name +
+                          " "+p.user.last_name, "name": p.portfolio_name, "value": idAndBalance[p.user_id]})
+
+        return jsonify(top_portfolios)
