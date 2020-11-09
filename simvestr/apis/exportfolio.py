e0ad6373a7f76d72037de39e1a661be8003c1ed7
kovidd <49589399+kovidd@users.noreply.github.com>
2020-11-10 00:06:27 +1100

    Update exportfolio.py

--- simvestr/apis/exportfolio.py
+++ simvestr/apis/exportfolio.py
@@ -1,20 +1,14 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Nov  1 01:08:54 2020
-
 @author: Kovid
 """
 
-from flask_restx import Resource, fields, reqparse, Namespace
-
+from flask_restx import Resource, Namespace
 from simvestr.helpers.auth import requires_auth, get_user
-from simvestr.helpers.portfolio import stock_balance
-from simvestr.models import db, Transaction, Stock
-from simvestr.apis.search import StockDetails
+from simvestr.models import Stock
 from simvestr.apis.portfolio import PortfolioQuery
 from simvestr.helpers.portfolio import portfolio_value
-
-import requests
 import xlsxwriter
 
 api = Namespace(
@@ -28,48 +22,43 @@ api = Namespace(
     description="Back-end API for exporting portfolio to csv file",
 )
 
+
 def create_csv(user, portfolio_details, portfolio_value_user):
     file_basename = f'{portfolio_details["portfolio_name"]}.xlsx'
     # path = 'simvestr\\helpers\\'
     path = ''
-    workbook = xlsxwriter.Workbook(f'{path+file_basename}')
+    workbook = xlsxwriter.Workbook(f'{path + file_basename}')
     worksheet = workbook.add_worksheet()
-    
+
     heading_format_1 = workbook.add_format({'bold': True, 'bg_color': '#28B463', 'font_color': 'black'})
     cell_format_1 = workbook.add_format({'bg_color': '#D2B4DE', 'font_color': 'black'})
-    
+
     heading_format_2 = workbook.add_format({'bold': True, 'bg_color': '#2980B9', 'font_color': 'black'})
     cell_format_2 = workbook.add_format({'bg_color': '#F7DC6F', 'font_color': 'black'})
-    
+
     worksheet.write('A1', 'Name', heading_format_1)
     worksheet.write('B1', f'{user.first_name} {user.last_name}', cell_format_1)
-    
+
     worksheet.write('A2', 'Date Joined', heading_format_2)
     worksheet.write('B2', f'{(user.date_joined).date()}', cell_format_2)
-    
+
     worksheet.write('A3', 'Balance', heading_format_1)
     worksheet.write('B3', f'{portfolio_details["balance"]}', cell_format_1)
 
     worksheet.write('A4', 'Total Value', heading_format_2)
     worksheet.write('B4', f'{portfolio_details["total_value"]}', cell_format_2)
-    
+
     worksheet.write('A5', 'Cash Balance', heading_format_1)
     worksheet.write('B5', f'{portfolio_details["total_value"] + portfolio_details["balance"]}', cell_format_1)
-    
-    print(f'Name, {user.first_name} {user.last_name}\n')
-    print(f'Date Joined, {(user.date_joined).date()}\n')
-    print(f'Balance, {portfolio_details["balance"]}\n')
-    print(f'Total Value, {portfolio_details["total_value"]}\n')
-    
-    
+
     # Add a format. Light red fill with dark red text.
     format1 = workbook.add_format({'bg_color': '#FFC7CE', 'font_color': '#9C0006'})
-    
+
     # Add a format. Green fill with dark green text.
     format2 = workbook.add_format({'bg_color': '#C6EFCE', 'font_color': '#006100'})
-    
+
     stock_heading_format = workbook.add_format({'bold': True, 'bg_color': '#5DADE2', 'font_color': 'black'})
-    
+
     row = 7
     worksheet.write(f'C{row}', 'Stocks', stock_heading_format)
     worksheet.write(f'D{row}', 'Symbol', stock_heading_format)
@@ -78,8 +67,8 @@ def create_csv(user, portfolio_details, portfolio_value_user):
     worksheet.write(f'G{row}', 'Value', stock_heading_format)
     worksheet.write(f'H{row}', 'Weighted Avg Fee', stock_heading_format)
     worksheet.write(f'I{row}', 'Weighted Average', stock_heading_format)
-    
-    for symbol, details  in portfolio_value_user.items():
+
+    for symbol, details in portfolio_value_user.items():
         row += 1
         stock = Stock.query.filter_by(symbol=symbol).first()
         worksheet.write(f'C{row}', f'{stock.name}')
@@ -93,8 +82,9 @@ def create_csv(user, portfolio_details, portfolio_value_user):
             worksheet.write(f'I{row}', f'{details["buy"]["weighted_average"]}', format1)
         worksheet.write(f'G{row}', f'{details["value"]}')
         worksheet.write(f'H{row}', f'{details["buy"]["weighted_average_fee"]}')
-        
+
     workbook.close()
+
 
 @api.route("")
 class ExportPortfolio(Resource):
@@ -103,10 +93,10 @@ class ExportPortfolio(Resource):
     def get(self):
         user = get_user()  # get user details from token
         portfolio_details = PortfolioQuery.get(user.id)[0]
-        portfolio_value_user = portfolio_value(user) 
-                        
+        portfolio_value_user = portfolio_value(user)
+
         create_csv(user, portfolio_details, portfolio_value_user)
-        
+
         return (
             {
                 "message": "Portfolio downloaded",
