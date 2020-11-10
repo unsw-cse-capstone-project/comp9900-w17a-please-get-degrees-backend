2def2c873a8076413e2146a15241f36a12247122
kovidd <49589399+kovidd@users.noreply.github.com>
2020-11-11 01:31:24 +1100

    Adding model-info-models, changing error responses

--- simvestr/apis/balance.py
+++ simvestr/apis/balance.py
@@ -1,31 +1,15 @@
-from flask_restx import Resource, Namespace, fields
+from flask_restx import Resource, Namespace
 
 from simvestr.helpers.auth import requires_auth, get_user
-from simvestr.helpers.portfolio import calculate_all_portfolios_values
+from simvestr.helpers.api_models import balance_model
 
 api = Namespace('balance', description='Api for viewing balance for a User')
 
-
-balance_model = api.model(
-    "UserBalance",
-    {
-        "name": fields.String(
-            description="Portfolio name",
-            example="John Doe's Portfolio"
-        ),
-        "balance": fields.Float(
-            description="Current cash balance",
-            example=100000.0,
-        ),
-
-    },
-)
-
+api.models[balance_model.name] = balance_model
 
 @api.route("", doc=False)
 class PortfolioPriceUsersQuery(Resource):
     @api.response(200, 'Successful')
-    @api.response(602, 'Portfolio for this user doesn\'t exist')
     @requires_auth
     def get(self):
         user = get_user()
@@ -51,9 +35,10 @@ class PortfolioPriceUsersQuery(Resource):
 )
 class PortfolioPriceQuery(Resource):
     @api.response(200, "Successful")
-    @api.response(602, "Portfolio for this user doesn't exist") #needed??
     @api.doc(
-        model=balance_model
+        id="user_balance", 
+        model=balance_model, 
+        descriptions="User cash balance"
     )
     @requires_auth
     def get(self, ):
@@ -65,20 +50,3 @@ class PortfolioPriceQuery(Resource):
         )
 
         return data
-
-#TODO: Is this needed?
-@api.route("/user/detailed")
-class PortfolioPriceUserQuery(Resource):
-    @api.response(200, "Successful")
-    @api.response(404, "Portfolio for this user doesn't exist") #Needed?
-    @requires_auth
-    def get(self):
-        user = get_user()
-
-        data = dict(
-            user_id=user.id,
-            portfolio_name=user.portfolio.portfolio_name,
-            close_balance=user.portfolio.portfolioprice[-1].close_balance
-        )
-        payload = {user.portfolio.portfolioprice[-1].id: data}
-        return payload
