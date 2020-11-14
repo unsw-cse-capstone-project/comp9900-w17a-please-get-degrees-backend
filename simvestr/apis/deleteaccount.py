f19909bb6b5a61f2bd488d006b7b8c786a547665
simonxxxxx <testsg@cse.unsw.edu.au>
2020-11-14 20:41:48 +1100

    first working delete api

--- simvestr/apis/deleteaccount.py
+++ simvestr/apis/deleteaccount.py
@@ -1,7 +1,8 @@
 from flask_restx import Resource, Namespace, reqparse, abort
 from simvestr.helpers.auth import get_user, requires_auth
-from simvestr.models import User, Portfolio, Watchlist, db
+from simvestr.models import User, Portfolio, PortfolioPrice, Transaction, Watchlist, db
 from sqlalchemy.sql import select
+from flask import jsonify
 
 api = Namespace(
     "deleteaccount",
@@ -24,10 +25,39 @@ class DeleteAccount(Resource):
     )
     def get(self):
         user = get_user()
+        watch = user.watchlist
+        port = user.portfolio
+
+        for pp in PortfolioPrice.query.filter_by(portfolio_id=port.id).all() :
+            PortfolioPrice.query.filter_by(id=pp.id).delete()
+        for tr in Transaction.query.filter_by(portfolio_id=port.id).all() :
+            Transaction.query.filter_by(id=pp.id).delete()
+
         User.query.filter_by(id=user.id).delete()
+        Watchlist.query.filter_by(id=watch.id).delete()
+        Portfolio.query.filter_by(id=port.id).delete()       
+
         db.session.commit()
+
+        @after_this_request
+        def set_cookie_value(response):
+            response.delete_cookie("token")
+            return response
+      
         return (
-            {"message": "Account deleted", },
+            {"message": "Account Deleted", },
             200,
         )
+        # user = User.query.filter_by(id=4).first()
+        # watch = user.watchlist
+        # port = user.portfolio
+        # test =  str(user.id) + " "+ str(watch.id) + " "+ str(port.id) + " "
+        # for pp in PortfolioPrice.query.filter_by(portfolio_id=port.id).all() :
+        #     test = test + str(pp.id) + " "
+        # for tr in Transaction.query.filter_by(portfolio_id=port.id).all() :
+        #     test = test + str(tr.id) + " "
+        #return test
+
+
+    
     
