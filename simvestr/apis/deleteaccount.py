e3f4594550b457e6979fa2904b97493d3bc8fb29
kovidd <49589399+kovidd@users.noreply.github.com>
2020-11-15 10:55:09 +1100

    Update deleteaccount.py

--- simvestr/apis/deleteaccount.py
+++ simvestr/apis/deleteaccount.py
@@ -2,7 +2,7 @@ from flask_restx import Resource, Namespace, reqparse, abort
 from simvestr.helpers.auth import get_user, requires_auth
 from simvestr.models import User, Portfolio, PortfolioPrice, Transaction, Watchlist, db
 from sqlalchemy.sql import select
-from flask import jsonify
+
 
 api = Namespace(
     "deleteaccount",
@@ -15,13 +15,13 @@ api = Namespace(
     description="Back-end API for deleting users account",
 )
 
-@api.route("/")
+
+@api.route("")
 class DeleteAccount(Resource):
     @requires_auth
     @api.response(200, "Successful")
     @api.doc(
         description="deletes users account, watchlist and portfolio",
-        #model=delete_account_model,
     )
     def get(self):
         user = get_user()
@@ -39,7 +39,4 @@ class DeleteAccount(Resource):
 
         db.session.commit()
      
-        return (
-            {"message": "Account Deleted", },
-            200,
-        )
+        return 200
