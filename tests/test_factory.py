794385af207904e22f08ce5d6d491364b224e935
Khan Schroder-Turner <khan.schroder-turner@ghd.com>
2020-11-15 20:31:33 +1100

    test failure

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -4,8 +4,3 @@ from simvestr import create_app
 def test_config():
     assert not create_app().testing
     assert create_app({'TESTING': True}).testing
-
-
-def test_hello(client):
-    response = client.get('/')
-    assert response.data == b"Simvestr App"
