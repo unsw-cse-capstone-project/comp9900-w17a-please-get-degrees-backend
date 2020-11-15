811d9e128be4e84422536ba9003fcfcf48f782e9
Khan Schroder-Turner <khan.schroder-turner@ghd.com>
2020-11-15 21:15:17 +1100

    missing dependency

--- simvestr/__init__.py
+++ simvestr/__init__.py
@@ -10,9 +10,10 @@ from simvestr.helpers.utils import load_yaml_config
 from simvestr.models import db
 
 
-def boot_app(app):
+def boot_app(app, run_setup=False):
 
     db_path = Path(app.instance_path) / "simvestr.db"
+    print(db_path)
     app.config.from_mapping(
         SECRET_KEY="dev",
         DATABASE=db_path,
@@ -22,10 +23,11 @@ def boot_app(app):
 
     db.init_app(app)
 
-    if not db_path.is_file() :
+    if not db_path.is_file() or run_setup:
         print("Aah new installation!")
-        curr_dir = Path(__file__).parent.parent
+        curr_dir = Path(__file__).parent
         input_data = curr_dir / "resources" / "test_data_user.xlsx"
+        print(input_data)
         setup_new_db(app, input_data)
     else:
         print("Database file found, won't reset the db!")
@@ -47,45 +49,24 @@ def create_app(test_config=None, sim_config=None, run_setup=False):
     is_test_and_sim = is_sim and is_test
     is_production = not is_test_and_sim
     # ensure the instance folder exists
-    try:
-        Path(app.instance_path).mkdir(exist_ok=True)
-    except OSError:
-        pass
+
+    Path(app.instance_path).mkdir(exist_ok=True, parents=True)
 
     if is_production:
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile("config.py", silent=True)
-        boot_app(app)
+        boot_app(app, run_setup)
     elif is_test_and_sim:
         # load the test config if passed in
         app.config.from_mapping(test_config)
     elif is_test:
         app.config.from_mapping(test_config)
     else:
-        boot_app(app)
+        boot_app(app, run_setup)
 
     app.register_blueprint(api)
 
     CORS(app, supports_credentials=True)
 
-
-    #
-    # # Example how to add a simple route that renders a page
-    # @app.route("/")
-    # def index():
-    #     return "Simvestr App"
-
-    # update_config = dict(
-    #     duration=datetime.timedelta(weeks=10000),
-    #     interval=datetime.timedelta(seconds=2),
-    # )
-    # delay = get_delay()
-    # daily_update_thread = threading.Timer(
-    #     interval=delay,
-    #     function=update_portfolio,
-    #     kwargs=update_config,
-    # )
-    # daily_update_thread.daemon = True
-    # daily_update_thread.start()
 
     return app
